841ab78f3ec40287f9fe53e93429d05e63a4b8e0
hellskater <ksrinivasrao531@gmail.com>
2023-01-31 13:57:01 +0530

    build error

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -14,10 +14,35 @@ if (!existsSync(logDir) && NODE_ENV !== 'production') {
 // Define log format
 const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`);
 
-/*
- * Log Level
- * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
- */
+let transports = [
+  // error log setting
+  new winstonDaily({
+    level: 'error',
+    datePattern: 'YYYY-MM-DD',
+    dirname: logDir + '/error',
+    filename: `%DATE%.log`,
+    maxFiles: 30,
+    handleExceptions: true,
+    json: false,
+    zippedArchive: true,
+  }),
+];
+
+if (NODE_ENV !== 'production') {
+  transports.push(
+    // debug log setting
+    new winstonDaily({
+      level: 'debug',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/debug',
+      filename: `%DATE%.log`,
+      maxFiles: 30,
+      json: false,
+      zippedArchive: true,
+    })
+  );
+}
+
 const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp({
@@ -25,36 +50,16 @@ const logger = winston.createLogger({
     }),
     logFormat,
   ),
-  transports: [
-    // debug log setting
-    new winstonDaily({
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      json: false,
-      zippedArchive: true,
-    }),
-    // error log setting
-    new winstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/error', // log file /logs/error/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      handleExceptions: true,
-      json: false,
-      zippedArchive: true,
-    }),
-  ],
+  transports: transports,
 });
 
-logger.add(
-  new winston.transports.Console({
-    format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
-  }),
-);
+if (NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
+    }),
+  );
+}
 
 const stream = {
   write: (message: string) => {
