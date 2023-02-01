7607b4c21de0ab290002026bc7f6642b0588ba44
hellskater <ksrinivasrao531@gmail.com>
2023-02-01 17:06:24 +0530

    removed auth middleware from refresh flow

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -17,7 +17,7 @@ class AuthRoute implements Routes {
   private initializeRoutes() {
     this.router.post(`${this.path}signup`, validationMiddleware(CreateUserDto, 'body'), this.authController.signUp);
     this.router.post(`${this.path}login`, validationMiddleware(LoginUserDto, 'body'), this.authController.logIn);
-    this.router.post(`${this.path}refresh`, authMiddleware, this.authController.refreshToken);
+    this.router.post(`${this.path}refresh`, this.authController.refreshToken);
   }
 }
 
